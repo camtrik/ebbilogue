ee953e9b559515ccc9236c1b8cdcc299b4a8e8e5
cam trik <camtrikcsky@gmail.com>
2025-01-27 03:17:13 +0800

    add claimbox

--- components/home/DashBoard.tsx
+++ components/home/DashBoard.tsx
@@ -3,8 +3,10 @@ import IconBox from './IconBox'
 import SearchBox from './SearchBox'
 import ScrollTagsBox from './ScrollTagsBox'
 import ResponsiveBox from './ResponsiveBox'
+import ClaimBox from './ClaimBox'
 
 export default function DashBoard() {
+
   return (
     <div className="divid-y space-y-3">
       <div className="mt-6 flex justify-between space-x-3">
@@ -14,7 +16,7 @@ export default function DashBoard() {
           <SearchBox />
         </div> */}
       </div>
-
+      <ClaimBox />
       {/* <ResponsiveBox />
 
       <ScrollTagsBox /> */}
