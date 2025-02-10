3aaa71690496e894b4c3ebefa6525fe9809d1265
cam trik <camtrikcsky@gmail.com>
2025-02-10 20:35:36 +0800

    update

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,6 +2,7 @@ import siteMetadata from '@/data/siteMetadata'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 import DashBoard from '@/components/home/DashBoard'
 import RecentPosts from '@/components/home/RecentPosts'
+
 export default function Home() {
   return (
     <div className="space-y-10 divide-y divide-gray-200 dark:divide-gray-700">
